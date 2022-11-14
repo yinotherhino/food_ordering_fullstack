febcf8b5039db1925471edb67af03530ab18d5a8
yinotherhino <muhammedbayero@gmail.com>
2022-11-14 20:02:12 +0100

    gitignore

--- dist/config/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const sequelize_1 = require("sequelize");
-exports.db = new sequelize_1.Sequelize("app", "", "", {
-    storage: "./food.sqlite",
-    dialect: "sqlite",
-    logging: false,
-});
